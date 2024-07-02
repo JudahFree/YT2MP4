520bee49fbe3e9ef2438c057490d72b0f97c7658
JudahFree <judahjfree@gmail.com>
2024-07-02 11:48:17 -0400

    Moved <Tabs /> to their own folder and scripts

--- client/src/descript.tsx
+++ client/src/descript.tsx
@@ -1,4 +1,5 @@
 import styles from "./Descript.module.css";
+import { Tabs } from "./downloadPage/dwnMenu";
 
 export const jsonToXml = (json: any) => {
   const { thumbnail, title, uploader, like_count } = json;
@@ -15,26 +16,6 @@ export const jsonToXml = (json: any) => {
           <Tabs />
         </div>
       </div>
-    </div>
-  );
-};
-
-export const Tabs = () => {
-  return (
-    <div>
-      <ul className={styles.downloadNav} id="tabs" role="tablist">
-        <li className="nav-item" role="presentation">
-          <button className="nav-link active" id="mp4-tab" type="button">
-            MP4
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="mp3-tab" name="disabled">
-            MP3
-          </button>
-        </li>
-      </ul>
-      <div className="tab-content"></div>
     </div>
   );
 };

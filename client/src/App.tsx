af0563ba1fb90fe7f35fa939a75a4fbeb9676e22
JudahFree <judahjfree@gmail.com>
2024-07-02 12:21:50 -0400

    Added Loading Circle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 import { Footer } from "./footer";
 import { jsonToXml } from "./descript";
 import logo from "./assets/YTMP4.svg";
+import loadingSpinner from "./assets/icons8-Loading.png";
 
 function App() {
   return (
     <>
       <div>
         <img src={logo} id="logo" className="center" />
-        <h1 id="Title">YT2MP4</h1>
       </div>
       <div className="container-sm">
         <YTInput />
@@ -25,6 +25,7 @@ export function YTInput() {
   );
   const [error, setError] = useState("");
   const [xmlData, setXmlData] = useState<JSX.Element | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(event.target.value);
@@ -32,6 +33,7 @@ export function YTInput() {
 
   const handleButtonClick = async () => {
     if (url && url.startsWith("https://www.youtube.com/watch?v=")) {
+      setLoading(true);
       console.log(url);
       try {
         const response = await fetch("http://127.0.0.1:5000/info?url=" + url, {
@@ -53,6 +55,8 @@ export function YTInput() {
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
         setError("Failed to process the URL. Please try again later.");
+      } finally {
+        setLoading(false);
       }
     } else {
       setError(
@@ -74,6 +78,13 @@ export function YTInput() {
           Convert
         </button>
         <div id="error">{error && <p>{error}</p>}</div>
+        {loading && (
+          <img
+            src={loadingSpinner}
+            alt="Loading..."
+            className="loading-spinner"
+          />
+        )}
         <div id="xmlData">
           {xmlData && (
             <pre>
